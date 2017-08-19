ec5eda65b5d9796de1967281e5be844ce0a1db2e
robojones <jonathan.scholz-weil@t-online.de>
2017-08-19 02:42:51 +0200

    add ESDoc definitions

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+/**
+ * Promisify the original function.
+ * @param {original} original - Function that gets promisified.
+ * @param {?Object} self - Object that gets applied as this-object when the original function is called.
+ * @returns {wrapper}
+ */
 function promisify(original, self = null) {
   if (typeof original !== 'function') {
     throw new TypeError('original must be a function')
   }
 
+  /**
+   * Wrapped original function.
+   * @typedef {function} wrapper
+   * @param {...*} args - Arguments to apply to the original function.
+   * @returns {Promise.<*>} - Promise that gets resolved when the original function calls the callback.
+   */
   function wrapper(...args) {
     let thisArg = self
     if (this !== global) {
@@ -17,6 +29,12 @@ function promisify(original, self = null) {
 
     // promisified
     return new Promise((resolve, reject) => {
+      /**
+       * Callback for the original function.
+       * @typedef {function} cb
+       * @param {?Error} error - Error if one occured.
+       * @param {*} result - Result of the original function.
+       */
       function cb(error, result) {
         if (error) {
           return reject(error)
@@ -34,3 +52,10 @@ function promisify(original, self = null) {
 }
 
 module.exports = promisify
+
+/**
+ * Function to promisify.
+ * @typedef {function} original
+ * @param {...*} arguments - Arguments of the original function.
+ * @param {cb} cb - Callback for the original function.
+ */
